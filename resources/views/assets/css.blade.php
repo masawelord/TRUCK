ab55256aa46048a6a3363b366442e816fc416b67
Adevotus Dickson <73017158+adevotus@users.noreply.github.com>
2022-04-14 17:41:15 -0700

    second commit

---
+++ resources/views/assets/css.blade.php
@@ -0,0 +1,21 @@
+<meta charset="utf-8" />
+    <title>Transport Truck</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+
+    <!-- plugin css -->
+    <link href="{{asset('assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css')}}" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="{{asset('assets/css/config/default/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+    <link href="{{asset('assets/css/config/default/app.min.css')}}" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+
+    <link href="{{asset('assets/css/config/default/bootstrap-dark.min.css')}}" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" />
+    <link href="{{asset('assets/css/config/default/app-dark.min.css')}}" rel="stylesheet" type="text/css" id="app-dark-stylesheet" />
+
+     <!-- icons -->
+     <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
---
+++ resources/views/assets/js.blade.php
@@ -0,0 +1,16 @@
+<!-- Right bar overlay-->
+<div class="rightbar-overlay"></div>
+
+<!-- Vendor js -->
+<script src="{{asset('assets/js/vendor.min.js')}}"></script>
+
+<!-- Plugins js-->
+<script src="{{asset('assets/libs/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+<script src="{{asset('assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+<script src="{{asset('assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js')}}"></script>
+
+<!-- Dashboard 2 init -->
+<script src="{{asset('assets/js/pages/dashboard-2.init.js')}}"></script>
+
+<!-- App js-->
+<script src="{{asset('assets/js/app.min.js')}}"></script>
