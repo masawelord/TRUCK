d777d4ce40590e09582e617d2e3d267c9612a21a
Adevotus Dickson <73017158+adevotus@users.noreply.github.com>
2022-04-21 16:43:24 -0700

    homepage

--- resources/views/assets/homecss.blade.php
+++ resources/views/assets/homecss.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-    <title>Logistica - Shipping Company Website Template</title>
+    <title>Yarongo Enterpresis Company Limited</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
