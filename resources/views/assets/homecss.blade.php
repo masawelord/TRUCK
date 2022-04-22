9b31014b0001dbee024871c60ccbfbe5ee767d9a
Adevotus Dickson <73017158+adevotus@users.noreply.github.com>
2022-04-22 13:00:28 -0700

    --- Auto-Git Commit ---

--- resources/views/assets/homecss.blade.php
+++ resources/views/assets/homecss.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-    <title>Yarongo Enterpresis Company Limited</title>
+    <title>Yarongo Enterpresis</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
--- resources/views/layouts/home/footer.blade.php
+++ resources/views/layouts/home/footer.blade.php
@@ -4,9 +4,9 @@
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Address</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Dodoma City Tanzania</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+(255) 345 67890</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@yarongoenterprises.com</p>
                     <div class="d-flex pt-2">
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
