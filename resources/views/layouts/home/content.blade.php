e7bebd1b4226cc62f3264a2a736fa597fcd67686
Adevotus Dickson <73017158+adevotus@users.noreply.github.com>
2022-04-26 14:21:29 -0700

    --- Auto-Git Commit ---

--- resources/views/layouts/home/content.blade.php
+++ resources/views/layouts/home/content.blade.php
@@ -102,8 +102,8 @@
       </div>
   </div>
   <!-- Service End -->
-  
-  
+
+
   <!-- Fact Start -->
   <div class="container-xxl py-5">
       <div class="container py-5">
@@ -188,76 +188,66 @@
   <!-- Feature End -->
 
 
-  <!-- Pricing Start -->
+  <!-- blog start -->
   <div class="container-xxl py-5">
       <div class="container py-5">
           <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-              <h6 class="text-secondary text-uppercase">Pricing Plan</h6>
-              <h1 class="mb-5">Perfect Pricing Plan</h1>
+              <h6 class="text-secondary text-uppercase">Blogs & Event and News</h6>
+              <h1 class="mb-5">Explore Our event that are accour to us</h1>
           </div>
           <div class="row g-4">
+              <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                  <div class="service-item p-4">
+                      <div class="overflow-hidden mb-4">
+                          <img class="img-fluid" src="{{asset('home/img/service-1.jpg')}}" alt="">
+                      </div>
+                      <h4 class="mb-3">Air Freight</h4>
+                      <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                      <a class="btn btn-secondary text-center mt-2" href="">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+
+                        </a>
+                  </div>
+              </div>
+              <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.2s">
+                  <div class="service-item p-4">
+                      <div class="overflow-hidden mb-4">
+                          <img class="img-fluid" src="{{asset('home/img/service-2.jpg')}}" alt="">
+                      </div>
+                      <h4 class="mb-3">Ocean Freight</h4>
+                      <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                      <a class="btn btn-secondary text-center mt-2" href="">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+
+                        </a>
+                  </div>
+              </div>
               <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
-                  <div class="price-item">
-                      <div class="border-bottom p-4 mb-4">
-                          <h5 class="text-primary mb-1">Basic Plan</h5>
-                          <h1 class="display-5 mb-0">
-                              <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>49.00<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
-                          </h1>
+                  <div class="service-item p-4">
+                      <div class="overflow-hidden mb-4">
+                          <img class="img-fluid" src="{{asset('home/img/service-3.jpg')}}" alt="">
                       </div>
-                      <div class="p-4 pt-0">
-                          <p><i class="fa fa-check text-success me-3"></i>HTML5 & CSS3</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Bootstrap v5</p>
-                          <p><i class="fa fa-check text-success me-3"></i>FontAwesome Icons</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Responsive Layout</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Cross-browser Support</p>
-                          <a class="btn-slide mt-2" href=""><i class="fa fa-arrow-right"></i><span>Order Now</span></a>
-                      </div>
+                      <h4 class="mb-3">Road Freight</h4>
+                      <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                      <a class="btn btn-secondary text-center mt-2" href="">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+
+                        </a>
                   </div>
               </div>
-              <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
-                  <div class="price-item">
-                      <div class="border-bottom p-4 mb-4">
-                          <h5 class="text-primary mb-1">Standard Plan</h5>
-                          <h1 class="display-5 mb-0">
-                              <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>99.00<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
-                          </h1>
-                      </div>
-                      <div class="p-4 pt-0">
-                          <p><i class="fa fa-check text-success me-3"></i>HTML5 & CSS3</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Bootstrap v5</p>
-                          <p><i class="fa fa-check text-success me-3"></i>FontAwesome Icons</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Responsive Layout</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Cross-browser Support</p>
-                          <a class="btn-slide mt-2" href=""><i class="fa fa-arrow-right"></i><span>Order Now</span></a>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.7s">
-                  <div class="price-item">
-                      <div class="border-bottom p-4 mb-4">
-                          <h5 class="text-primary mb-1">Advanced Plan</h5>
-                          <h1 class="display-5 mb-0">
-                              <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>149.00<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
-                          </h1>
-                      </div>
-                      <div class="p-4 pt-0">
-                          <p><i class="fa fa-check text-success me-3"></i>HTML5 & CSS3</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Bootstrap v5</p>
-                          <p><i class="fa fa-check text-success me-3"></i>FontAwesome Icons</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Responsive Layout</p>
-                          <p><i class="fa fa-check text-success me-3"></i>Cross-browser Support</p>
-                          <a class="btn-slide mt-2" href=""><i class="fa fa-arrow-right"></i><span>Order Now</span></a>
-                      </div>
-                  </div>
-              </div>
+
           </div>
       </div>
   </div>
-  <!-- Pricing End -->
+  <!-- blog end End -->
+
 
 
   <!-- Quote Start -->
-  <div class="container-xxl py-5">
+  <div class="container-xxl py-5" id="requestTruck">
       <div class="container py-5">
           <div class="row g-5 align-items-center">
               <div class="col-lg-5 wow fadeInUp" data-wow-delay="0.1s">
--- resources/views/layouts/home/navigation.blade.php
+++ resources/views/layouts/home/navigation.blade.php
@@ -8,21 +8,11 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="#" class="nav-item nav-link active">Home</a>
+                <a href="/" class="nav-item nav-link">Home</a>
                 <a href="#" class="nav-item nav-link">About</a>
                 <a href="#" class="nav-item nav-link">Services</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu fade-up m-0">
-                        <a href="#" class="dropdown-item">Pricing Plan</a>
-                        <a href="#" class="dropdown-item">Features</a>
-                        <a href="#" class="dropdown-item">Free Quote</a>
-                        <a href="#" class="dropdown-item">Our Team</a>
-                        <a href="#" class="dropdown-item">Testimonial</a>
-                        <a href="#" class="dropdown-item">404 Page</a>
-                    </div>
-                </div>
-                <a href="#" class="nav-item nav-link">Contact</a>
+                <a href="#" class="nav-item nav-link">Blogs</a>
+                <a href="/contact" class="nav-item nav-link">Contact</a>
             </div>
             <div class="text-center text-secondary p-3 mr-2  text-black" style=" margin-right: 5%; background-color:cornflowerblue; font-weight:bold; margin-left:5%;box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;">
                 <a href="#" type="button" class="text-uppercase  p-2" style="font-size: small; ">Login/Register</a>
